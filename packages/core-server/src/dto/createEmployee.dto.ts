f3ee16c08cbc98c7516eebf3979fcf8ec360291e
barrera77 <barrera_ml@hotmail.com>
2025-04-29 00:24:55 -0600

    Finished Create Employee DTO

--- packages/core-server/src/dto/createEmployee.dto.ts
+++ packages/core-server/src/dto/createEmployee.dto.ts
@@ -1,3 +1,19 @@
 import { IsNotEmpty, IsMongoId, IsOptional, IsString } from 'class-validator';
 
-export class CreateEmployeeDto {}
+export class CreateEmployeeDto {
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @IsMongoId()
+  @IsNotEmpty()
+  departmentId: string;
+
+  @IsMongoId()
+  @IsOptional()
+  user?: string;
+}
